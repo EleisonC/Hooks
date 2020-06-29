736728c133b9243201bef166475777938aaf14d9
christopher kalule <christopherkalule@christophers-MacBook-Pro.local>
2020-06-29 15:14:45 +0200

    Updates

---
+++ precache-manifest.c07f0110e50be30f963aece367bd76d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d9d47fb22e2d2e096af37cd96faa157",
+    "url": "/Hooks/index.html"
+  },
+  {
+    "revision": "5aead58a1e2418811d6a",
+    "url": "/Hooks/static/css/main.d3aaa3ee.chunk.css"
+  },
+  {
+    "revision": "e76f96904575a4a163ff",
+    "url": "/Hooks/static/js/2.cdcd0f08.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Hooks/static/js/2.cdcd0f08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5aead58a1e2418811d6a",
+    "url": "/Hooks/static/js/main.6416f5f3.chunk.js"
+  },
+  {
+    "revision": "9acc6f7abcfc8d576e3f",
+    "url": "/Hooks/static/js/runtime-main.7d636bfe.js"
+  }
+]);
